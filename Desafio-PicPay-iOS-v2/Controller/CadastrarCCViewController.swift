206e1a47631fbc1db74797ce1397db4a432c3b22
Kelvin Batista Machado <kelvinmachado@live.com>
2020-08-11 19:22:06 -0300

    Corrige a atualização das constraints do botão salvar

--- Desafio-PicPay-iOS-v2/Controller/CadastrarCCViewController.swift
+++ Desafio-PicPay-iOS-v2/Controller/CadastrarCCViewController.swift
@@ -9,17 +9,64 @@
 import UIKit
 import SkyFloatingLabelTextField
 
-class CadastrarCCViewController: UIViewController {
+class CadastrarCCViewController: UIViewController, UITextFieldDelegate {
 
-    @IBOutlet weak var salvarBtn: UIButton!
+    let saveBtn = UIButton()
+
     @IBOutlet weak var numCartao: SkyFloatingLabelTextField!
     @IBOutlet weak var nomeTitular: SkyFloatingLabelTextField!
     @IBOutlet weak var vencimento: SkyFloatingLabelTextField!
     @IBOutlet weak var cvv: SkyFloatingLabelTextField!
 
+    var constraintBtnTop: NSLayoutConstraint!
+    var constraintBtnBottom: NSLayoutConstraint!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        salvarBtn.layer.cornerRadius = 25
+
+        setupSaveButton()
+        vencimento.delegate = self
+        cvv.delegate = self
+    }
+
+    func setupSaveButton() {
+        saveBtn.titleLabel?.font = UIFont(name: "Helvetica Neue", size: 16)
+        saveBtn.backgroundColor = #colorLiteral(red: 0, green: 0.7958126664, blue: 0.3956114948, alpha: 1)
+        saveBtn.setTitle("Salvar", for: .normal)
+        saveBtn.addTarget(self, action: #selector(saveBtnTapped), for: .touchUpInside)
+        view.addSubview(saveBtn)
+        setsaveBtnConstraints()
+    }
+    @objc func saveBtnTapped() {
+
+        print("Botão salvar pressionado")
+    }
+
+    func  setsaveBtnConstraints() {
+
+        saveBtn.translatesAutoresizingMaskIntoConstraints = false
+        saveBtn.layer.cornerRadius = 25
+
+        NSLayoutConstraint.activate([
+            saveBtn.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            saveBtn.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            saveBtn.heightAnchor.constraint(equalToConstant: 50)
+        ])
+
+        constraintBtnBottom = saveBtn.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -20)
+        constraintBtnBottom.isActive = true
+        constraintBtnTop = saveBtn.topAnchor.constraint(equalTo: vencimento.bottomAnchor, constant: 20)
+        constraintBtnTop.isActive = false
+    }
+
+    internal func textFieldDidBeginEditing(_ textField: UITextField) {
+        constraintBtnBottom.isActive = false
+        constraintBtnTop.isActive = true
+    }
+
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        constraintBtnBottom.isActive = true
+        constraintBtnTop.isActive = false
     }
 
 // MARK: - Keyboard
